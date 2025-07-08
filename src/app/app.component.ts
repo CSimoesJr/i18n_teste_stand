4ad0fe82dd9d279116649680ba9c8e0c338b4122
CSimoesJr <ju_csimoes@hotmail.com>
2025-07-08 11:09:09 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PoButtonModule, PoI18nService, PoPageModule, PoWidgetModule } from '@po-ui/ng-components';
+import { defaultContext } from './app.config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PoPageModule, PoButtonModule, PoWidgetModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'teste-i18n-stand';
+  title = 'teste-i18n';
+  literals: any = '';
+
+  constructor(private poI18nService: PoI18nService) {
+    this.loadTranslations(defaultContext);
+  }
+
+  title2: string = '';
+  title3: string = '';
+  description: string = '';
+  selectedLanguage: string = 'pt-BR';
+  translations: { [key: string]: string } = {};
+  message: string = '';
+  message2: string = '';
+
+  changeLanguage(language: string, context: string): void {
+    this.selectedLanguage = language;
+    this.poI18nService.setLanguage(language);
+    console.log('Estou aqui de novo', context);
+    this.loadTranslations(context);
+    console.log('Estou aqui de novo', language);
+  }
+
+  loadTranslations(context?: string): void {
+    this.poI18nService
+      .getLiterals({ context: context })
+      .subscribe((literals) => {
+        this.literals = literals;
+        console.log('app: ', literals);
+      });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,48 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { PoHttpRequestModule, PoI18nConfig, PoI18nModule } from '@po-ui/ng-components';
+import { PoUiI18nLibModule } from 'po-ui-i18n-lib';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+export const defaultContext = 'general';
+const browserLanguage = navigator.language || 'pt';
+const i18nConfig: PoI18nConfig = {
+  default: {
+    language: browserLanguage,
+    context: defaultContext,
+    cache: false,
+  },
+  contexts: {
+    general: {
+      en: {
+        teste: 'Texto do en-us App Final AAAA',
+        str0003: 'App text',
+      },
+      pt: {
+        teste: 'Texto do pt App Final',
+        str0003: 'sttr 0003 PT',
+      },
+    },
+    teste: {
+      en: {
+        teste: 'Texto do en-us App FinalLLLL',
+        str0003: 'App text',
+      },
+    },
+  },
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    importProvidersFrom([PoHttpRequestModule, PoUiI18nLibModule, PoI18nModule.config(i18nConfig)]),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
 };
